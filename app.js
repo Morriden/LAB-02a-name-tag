716f7be3117148245f4e400723e4205a3a5a99fb
Erik Ford <ekford18@gmail.com>
2020-03-26 17:42:44 -0700

    added button with name change

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const myPTag = document.getElementById('name')
-myPTag.textContent = 'Morriden'
 myPTag.style.fontSize = '30px'
 myPTag.style.fontFamily = 'fantasy'
 
-//ahhhhhh
+//when i click a button
+
+//i want the name to change to 'Fredy Mercury' and make the text silver
+
+const myButton = document.getElementById('myButton')
+myButton.addEventListener('click', function() {
+    const myinput = document.getElementById('inputbox');
+    myPTag.textContent = myinput.value;
+    myPTag.style.color = 'silver';
+    console.log('change the name!', Date.now());
+})
